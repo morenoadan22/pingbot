0e2570f081fcdfc9b965b72d818996f1874caa94
Adan Moreno <morenoadan22@gmail.com>
2017-08-24 17:07:48 -0700

    change to botkit

--- pingbot.js
+++ pingbot.js
@@ -1,18 +1,12 @@
 
-var Bot = require('slackbots')
+var Botkit = require('botkit')
 var config = require('./config/config.json')
 
-var bot = new Bot({ 
-	token: config.slack_api_token,
-	name: config.slack_bot_name
-})
+var controller = Botkit.slackbot(config)
 
-bot.on('start', function(){
-	// Let's make it respond to 
-	// '/pingbot pingtime'
-	// '/pingbot challenge <username>'
-	// '/pingbot new <user2> <[myscore : user2_score]>' --> sends a confirmation message to user2
-	// '/pingbot leaderboard'
-	// some more should come later, but should be good for now
-
-});
+// Let's make it respond to 
+// '/pingbot pingtime'
+// '/pingbot challenge <username>'
+// '/pingbot new <user2> <[myscore : user2_score]>' --> sends a confirmation message to user2
+// '/pingbot leaderboard'
+// some more should come later, but should be good for now
